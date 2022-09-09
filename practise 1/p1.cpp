afa65af6b695f3050877d3c68e3d9cfdd007e554
Villgarc <ivgvillgarc@gmail.com>
2022-09-09 11:02:53 +0200

    First commit

--- practise 1/p1.cpp
+++ practise 1/p1.cpp
@@ -4,11 +4,11 @@
 #include <climits>
 
 int main(){
-    std::cout << "The char maximum size is " << CHAR_MAX << '\n';
-    std::cout << "The char minimum size is " << CHAR_MIN << '\n';
-    std::cout << "The int maximum size is " << INT_MAX << '\n';
-    std::cout << "The int minimum size is " << INT_MIN << '\n';
-    std::cout << "The long int maximum size is " << LONG_MAX << '\n';
-    std::cout << "The long int minimum size is " << LONG_MIN << '\n';
+    std::cout << "The char maximum value is " << CHAR_MAX << '\n';
+    std::cout << "The char minimum value is " << CHAR_MIN << '\n';
+    std::cout << "The int maximum value is " << INT_MAX << '\n';
+    std::cout << "The int minimum value is " << INT_MIN << '\n';
+    std::cout << "The long int maximum value is " << LONG_MAX << '\n';
+    std::cout << "The long int minimum value is " << LONG_MIN << '\n';
     return 0;
 }
